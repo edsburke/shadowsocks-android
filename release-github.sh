240588d9257dcfc557f28bdd5f1f7e4f00d9077b
pexcn <i@pexcn.me>
2018-08-05 16:22:57 +0800

    auto release for circleci

---
+++ release-github.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+prepare() {
+  export TZ=Asia/Shanghai
+
+  git clone https://github.com/pexcn/shadowsocks-android.git -b release --depth 3 shadowsocks-release
+  pushd shadowsocks-release
+  git config user.name "pexcn"
+  git config user.email "i@pexcn.me"
+  git config log.date iso
+  find . ! -path . | grep -vE ".git|.circleci" | xargs rm
+  popd
+}
+
+release() {
+  pushd shadowsocks-release
+  cp ../mobile/build/outputs/apk/release/mobile-armeabi-v7a-release.apk shadowsocks-armeabi-v7a-release.apk
+  cp ../mobile/build/outputs/apk/release/mobile-arm64-v8a-release.apk shadowsocks-arm64-v8a-release.apk
+  cp ../mobile/build/outputs/apk/release/mobile-x86-release.apk shadowsocks-x86-release.apk
+  cp ../mobile/build/outputs/apk/release/mobile-universal-release.apk shadowsocks-universal-release.apk
+  popd
+}
+
+push() {
+  pushd shadowsocks-release
+  git add --all
+  git commit -m "`date +'%Y-%m-%d %T'`"
+  git push --quiet "https://${GITHUB_TOKEN}@github.com/pexcn/shadowsocks-android.git" HEAD:release
+  popd
+}
+
+prepare
+release
+push
